0b7c39490c303fdf5129c01f4b2b3a873f27568d
lazdex <1009822947@qq.com>
2018-01-07 23:13:51 +0800

    First commit.

---
+++ app/src/main/java/com/example/lazdex/hushuobadao/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lazdex.hushuobadao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
